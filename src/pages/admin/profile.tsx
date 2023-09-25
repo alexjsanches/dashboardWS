6772e4d83dd4c3e80a01071e5da55eb6a32c8350
alexjsanches <alexandrej.sanches@gmail.com>
2023-09-25 08:01:08 -0300

    Alterados os dias

--- src/pages/admin/profile.tsx
+++ src/pages/admin/profile.tsx
@@ -20,7 +20,7 @@ export default function ProfileOverview() {
     const metaUdi = 3129282.73;
     const metaGyn = 3824678.76; 
     const diasUteisNoMes = 20;
-    const diasConcluidos = 14;
+    const diasConcluidos = 15;
     const diasFaltantes = diasUteisNoMes - diasConcluidos;
     const tendenciaUDI = (udiSFormat / diasConcluidos) * diasUteisNoMes; 
     const tendenciaGYN = (gynSFormat / diasConcluidos) * diasUteisNoMes;
