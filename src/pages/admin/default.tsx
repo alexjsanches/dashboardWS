3e40697b2edfca609d3d1847e759641ec0b214c8
Alexandre Sanches <alexandrej.sanches@gmail.com>
2023-11-03 08:22:03 -0300

    das

--- src/pages/admin/default.tsx
+++ src/pages/admin/default.tsx
@@ -37,8 +37,8 @@ export default function UserReports() {
   const [gynSFormatHj, setGynSFormatHj] = useState<number | null>(null);
   const [udiSFormatGeral, setUdiSFormatGeral] = useState<number | null>(null);
   const [gynSFormatGeral, setGynSFormatGeral] = useState<number | null>(null);
-  const metaUdi =  3723000;  
-  const metaGyn =  3577000;
+  const metaUdi =  3577000;  
+  const metaGyn =  3723000;
   const diasUteisNoMes = 20;
   const diasConcluidos = 1;//
   const diasFaltantes = diasUteisNoMes - diasConcluidos;
