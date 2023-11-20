efac57984e3b166bf752b7bf482ce1531320ee97
Alexandre Sanches <alexandrej.sanches@gmail.com>
2023-11-20 07:00:29 -0300

    dadkjsah

--- src/pages/admin/default.tsx
+++ src/pages/admin/default.tsx
@@ -40,7 +40,7 @@ export default function UserReports() {
   const metaUdi =  3577000;  
   const metaGyn =  3723000;
   const diasUteisNoMes = 20;
-  const diasConcluidos = 10;//
+  const diasConcluidos = 11;//
   const diasFaltantes = diasUteisNoMes - diasConcluidos;
   const metaDiariaCalcUDI =
     udiSFormat != 0 ? (metaUdi - udiSFormatGeral) / diasFaltantes : 0;
